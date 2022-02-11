422fb65f4aab0f6b4a240d0f7491149abf528d32
LiZhi <li3zhi4@163.com>
2022-02-11 17:42:47 +0800

    Update 互相关注问题.sql

--- 互相关注问题.sql
+++ 互相关注问题.sql
@@ -5,7 +5,7 @@
 --             1     , 3
 --
 --   out       1 , 2
--- 找出所有互相关注的人
+-- 找出所有互相关注的人,要求不能有子查询
 select if(fromid<toid,fromid,toid) minid,if(fromid<toid,toid,fromid) maxid from tb group by minid,maxid having count(1)=2;
 
 -- 如果互相关注 ,一定是互逆的，那么将两个id 无差别排序，则改组中一定会存在两个
